1896b5720e46dffe37b4d39e778a305dd292ccf4
BagusJatikusuma <jatikusuma0801@gmail.com>
2017-09-11 12:42:47 +0700

    add jabatan and unit in pegawaiCredential

--- src/main/java/com/pemda/ekinerjademo/wrapper/output/PegawaiCredential.java
+++ src/main/java/com/pemda/ekinerjademo/wrapper/output/PegawaiCredential.java
@@ -10,13 +10,23 @@ public class PegawaiCredential {
     private String namaPegawai;
     private Role role;
     private String token;
+    private String jabatan;
+    private String unit;
 
     public PegawaiCredential() {}
-    public PegawaiCredential(String nipPegawai, String namaPegawai, Role role, String token) {
+    public PegawaiCredential(
+            String nipPegawai,
+            String namaPegawai,
+            Role role,
+            String token,
+            String jabatan,
+            String unit) {
         this.nipPegawai = nipPegawai;
         this.namaPegawai = namaPegawai;
         this.role = role;
         this.token = token;
+        this.jabatan = jabatan;
+        this.unit = unit;
     }
 
     public String getNipPegawai() {
@@ -49,5 +59,21 @@ public class PegawaiCredential {
 
     public void setRole(Role role) {
         this.role = role;
+    }
+
+    public String getJabatan() {
+        return jabatan;
+    }
+
+    public void setJabatan(String jabatan) {
+        this.jabatan = jabatan;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
     }
 }
