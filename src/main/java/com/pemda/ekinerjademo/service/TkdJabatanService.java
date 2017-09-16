5e2af1e1794d035bbe05c5be4f1c172c9c7976f8
BagusJatikusuma <jatikusuma0801@gmail.com>
2017-09-16 15:55:16 +0700

    add get jabatan by unit kerja

--- src/main/java/com/pemda/ekinerjademo/service/TkdJabatanService.java
+++ src/main/java/com/pemda/ekinerjademo/service/TkdJabatanService.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface TkdJabatanService {
     TkdJabatan getTkdJabatan(String kdJabatan);
     List<TkdJabatan> getAll();
+    List<TkdJabatan> getJabatanByUnitKerja(String kdUnitKerja);
 }
