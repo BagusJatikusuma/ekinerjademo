5d9a21ea147e8a5dffe8935ae389029e7edb6ac1
bayuarafli <bayu.arafli.tif14@polban.ac.id>
2017-10-30 13:37:21 +0700

    Pengambilan ajuan data uraian tugas dari semua pegawai berdasarkan Unit Kerja (controller)

--- src/main/java/com/pemda/ekinerjademo/controller/api/UraianTugasPegawaiTahunanController.java
+++ src/main/java/com/pemda/ekinerjademo/controller/api/UraianTugasPegawaiTahunanController.java
@@ -1,11 +1,12 @@
 package com.pemda.ekinerjademo.controller.api;
 
+import com.pemda.ekinerjademo.model.bismamodel.QutPegawai;
 import com.pemda.ekinerjademo.model.ekinerjamodel.UraianTugasPegawaiTahunan;
 import com.pemda.ekinerjademo.model.ekinerjamodel.UraianTugasPegawaiTahunanId;
+import com.pemda.ekinerjademo.service.QutPegawaiService;
 import com.pemda.ekinerjademo.service.UraianTugasPegawaiTahunanService;
 import com.pemda.ekinerjademo.wrapper.input.UraianTugasPegawaiTahunanInputWrapper;
-import com.pemda.ekinerjademo.wrapper.output.CustomMessage;
-import com.pemda.ekinerjademo.wrapper.output.UraianTugasPegawaiTahunanWrapper;
+import com.pemda.ekinerjademo.wrapper.output.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,7 @@ public class UraianTugasPegawaiTahunanController {
     public static final Logger LOGGER = LoggerFactory.getLogger(UraianTugasPegawaiTahunanController.class);
 
     @Autowired private UraianTugasPegawaiTahunanService urtugPegawaiTahunanService;
+    @Autowired private QutPegawaiService qutPegawaiService;
 
     @RequestMapping(value = "/get-uraian-tugas-pegawai-tahunan-by-nip/{nipPegawai}", method = RequestMethod.GET)
     ResponseEntity<?> getUraianTugasPegawaiTahunanByNip(@PathVariable("nipPegawai") String nipPegawai) {
@@ -117,5 +119,71 @@ public class UraianTugasPegawaiTahunanController {
             }
         }
         return new ResponseEntity<Object>(new CustomMessage("Uraian tugas yang dipilih telah disetujui"), HttpStatus.OK);
+    }
+
+    //Pengambilan ajuan data uraian tugas dari semua pegawai berdasarkan Unit Kerja
+    @RequestMapping(value = "/get-uraian-tugas-tahunan-by-unit-kerja/{kdUnitKerja}", method = RequestMethod.GET)
+    ResponseEntity<?> getUraianTugasTahunanByUnitKerja(@PathVariable("kdUnitKerja") String kdUnitKerja) {
+        LOGGER.info("get uraian tugas tahunan by unit kerja");
+
+        List<UraianTugasPegawaiTahunan> uraianTugasPegawaiTahunanList = urtugPegawaiTahunanService.getByUnitKerja(kdUnitKerja);
+        List<PegawaiCredential> pegawaiCredentialList = new ArrayList<>();
+        List<AjuanUraianTugasNonDpaPegawaiWrapper> ajuanUraianTugasNonDpaPegawaiWrapperList = new ArrayList<>();
+        List<QutPegawai> qutPegawaiList = qutPegawaiService.getQutPegawaiByUnitKerja(kdUnitKerja);
+
+        //looping pegawai yang ada pada uraian tugas tahunan
+        for (UraianTugasPegawaiTahunan uraianTugasPegawaiTahunan : uraianTugasPegawaiTahunanList){
+            Boolean constraint = false;
+
+            for (PegawaiCredential pegawaiCredential : pegawaiCredentialList) {
+                if (pegawaiCredential.getNipPegawai()
+                        .equals(uraianTugasPegawaiTahunan
+                                .getUraianTugasPegawaiTahunanId()
+                                .getNipPegawai())){
+                    constraint = true;
+                    break;
+                }
+            }
+
+            if (!constraint){
+                pegawaiCredentialList.add(new PegawaiCredential(
+                        uraianTugasPegawaiTahunan.getUraianTugasPegawaiTahunanId().getNipPegawai()
+                        ,null,null,null,null,null,null));
+            }
+        }
+
+        //insert data pegawai credential kedalam ajuan uraian tugas non dpa list
+        for (PegawaiCredential pegawaiCredential : pegawaiCredentialList){
+            for (QutPegawai qutPegawai : qutPegawaiList){
+                if (qutPegawai.getNip().equals(pegawaiCredential.getNipPegawai())){
+                    AjuanUraianTugasNonDpaPegawaiWrapper ajuanUraianTugasNonDpaPegawaiWrapper = new AjuanUraianTugasNonDpaPegawaiWrapper();
+                    ajuanUraianTugasNonDpaPegawaiWrapper.setNamaPegawai(qutPegawai.getNip());
+                    ajuanUraianTugasNonDpaPegawaiWrapper.setNamaPegawai(qutPegawai.getNama());
+                    ajuanUraianTugasNonDpaPegawaiWrapper.setNamaPegawai(qutPegawai.getKdJabatan());
+                    ajuanUraianTugasNonDpaPegawaiWrapper.setNamaPegawai(qutPegawai.getJabatan());
+
+                    ajuanUraianTugasNonDpaPegawaiWrapperList.add(ajuanUraianTugasNonDpaPegawaiWrapper);
+                }
+            }
+        }
+
+        //insert urtug ke setiap elemen tugas
+        for (AjuanUraianTugasNonDpaPegawaiWrapper ajuanUraianTugasNonDpaPegawaiWrapper : ajuanUraianTugasNonDpaPegawaiWrapperList){
+            List<UraianTugasWrapper> uraianTugasWrapperList = new ArrayList<>();
+
+            for (UraianTugasPegawaiTahunan uraianTugasPegawaiTahunan : uraianTugasPegawaiTahunanList){
+                if (ajuanUraianTugasNonDpaPegawaiWrapper.getNipPegawai().equals(uraianTugasPegawaiTahunan.getUraianTugasPegawaiTahunanId().getNipPegawai())) {
+                    uraianTugasWrapperList.add(
+                            new UraianTugasWrapper(
+                                    uraianTugasPegawaiTahunan.getUraianTugasPegawaiTahunanId().getKdUrtug(),
+                                    uraianTugasPegawaiTahunan.getUraianTugasJabatanJenisUrtug().getUraianTugasJabatan().getUraianTugas().getDeskripsi()));
+                }
+            }
+
+            ajuanUraianTugasNonDpaPegawaiWrapper.setUraianTugasDiajukan(uraianTugasWrapperList);
+        }
+
+        return new ResponseEntity<Object>(ajuanUraianTugasNonDpaPegawaiWrapperList, HttpStatus.OK);
+
     }
 }
---
+++ src/main/java/com/pemda/ekinerjademo/wrapper/output/AjuanUraianTugasNonDpaPegawaiWrapper.java
@@ -0,0 +1,56 @@
+package com.pemda.ekinerjademo.wrapper.output;
+
+import java.util.List;
+
+/**
+ * Created by bayu on 30/10/17.
+ */
+public class AjuanUraianTugasNonDpaPegawaiWrapper {
+
+    private String nipPegawai;
+    private String namaPegawai;
+    private String kdJabatan;
+    private String jabatan;
+    private List<UraianTugasWrapper> uraianTugasDiajukan;
+
+    public List<UraianTugasWrapper> getUraianTugasDiajukan() {
+        return uraianTugasDiajukan;
+    }
+
+    public void setUraianTugasDiajukan(List<UraianTugasWrapper> uraianTugasDiajukan) {
+        this.uraianTugasDiajukan = uraianTugasDiajukan;
+    }
+
+    public String getNipPegawai() {
+        return nipPegawai;
+    }
+
+    public void setNipPegawai(String nipPegawai) {
+        this.nipPegawai = nipPegawai;
+    }
+
+    public String getNamaPegawai() {
+        return namaPegawai;
+    }
+
+    public void setNamaPegawai(String namaPegawai) {
+        this.namaPegawai = namaPegawai;
+    }
+
+    public String getKdJabatan() {
+        return kdJabatan;
+    }
+
+    public void setKdJabatan(String kdJabatan) {
+        this.kdJabatan = kdJabatan;
+    }
+
+    public String getJabatan() {
+        return jabatan;
+    }
+
+    public void setJabatan(String jabatan) {
+        this.jabatan = jabatan;
+    }
+
+}
