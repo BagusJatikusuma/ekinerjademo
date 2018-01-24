97d823b014ebc664e8a118a920b62f58bb08f043
Bayu Arafli Fauzi <bayuaraflifauzi@gmail.com>
2018-01-24 16:39:03 +0700

    request mapping (surat dinas, surat edaran, surat keputusan, surat keterangan)

--- src/main/java/com/pemda/ekinerjademo/controller/api/SuratEdaranController.java
+++ src/main/java/com/pemda/ekinerjademo/controller/api/SuratEdaranController.java
@@ -1,14 +1,14 @@
 package com.pemda.ekinerjademo.controller.api;
 
 import com.pemda.ekinerjademo.service.SuratEdaranService;
+import com.pemda.ekinerjademo.wrapper.input.SuratEdaranInputWrapper;
+import com.pemda.ekinerjademo.wrapper.output.CustomMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by bayu on 18/01/18.
@@ -21,73 +21,83 @@ public class SuratEdaranController {
 
     @Autowired private SuratEdaranService suratEdaranService;
 
-    ResponseEntity<?> createSuratEdaran() {
+    @RequestMapping(value = "/create-surat-edaran", method = RequestMethod.POST)
+    ResponseEntity<?> createSuratEdaran(@RequestBody SuratEdaranInputWrapper inputWrapper) {
         LOGGER.info("create surat edaran");
 
-        return new ResponseEntity<Object>(null, HttpStatus.OK);
+        return new ResponseEntity<Object>(new CustomMessage("surat edaran created"), HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> sebarSuratEdaran() {
+    @RequestMapping(value = "/sebar-surat-edaran/{kdSuratEdaran}", method = RequestMethod.PUT)
+    ResponseEntity<?> sebarSuratEdaran(@PathVariable("kdSuratEdaran") String kdSuratEdaran) {
+        LOGGER.info("sebar surat edaran");
+
+        return new ResponseEntity<Object>(new CustomMessage("surat edaran telah disebar"), HttpStatus.OK);
+
+    }
+
+    @RequestMapping(value = "/approve-surat-edaran/{kdSuratEdaran}", method = RequestMethod.PUT)
+    ResponseEntity<?> approveSuratEdaran(@PathVariable("kdSuratEdaran") String kdSuratEdaran) {
         LOGGER.info("sebar surat edaran");
 
         return new ResponseEntity<Object>(null, HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> approveSuratEdaran() {
-        LOGGER.info("sebar surat edaran");
-
-        return new ResponseEntity<Object>(null, HttpStatus.OK);
-
-    }
-
-    ResponseEntity<?> terimaSuratEdaran() {
+    @RequestMapping(value = "/terima-surat-edaran/{kdSuratEdaran}", method = RequestMethod.PUT)
+    ResponseEntity<?> terimaSuratEdaran(@PathVariable("kdSuratEdaran") String kdSuratEdaran) {
         LOGGER.info("terima surat edaran");
 
         return new ResponseEntity<Object>(null, HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> getDaftarSuratEdaranHistoryByPegawai() {
+    @RequestMapping(value = "/get-daftar-surat-edaran-by-pembuat/{nipPembuat}", method = RequestMethod.GET)
+    ResponseEntity<?> getDaftarSuratEdaranHistoryByPegawai(@PathVariable("nipPembuat") String nipPembuat) {
         LOGGER.info("get surat edaran history");
 
         return new ResponseEntity<Object>(null, HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> getDaftarSuratEdaranTarget() {
+    @RequestMapping(value = "/get-daftar-surat-edaran-by-target/{kdJabatanTarget}", method = RequestMethod.GET)
+    ResponseEntity<?> getDaftarSuratEdaranTarget(@PathVariable("kdJabatanTarget") String kdJabatanTarget) {
         LOGGER.info("get surat edaran target");
 
         return new ResponseEntity<Object>(null, HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> getDaftarSuratEdaranTargetUnread() {
+    @RequestMapping(value = "/get-daftar-surat-edaran-by-target-unread/{kdJabatanTarget}", method = RequestMethod.GET)
+    ResponseEntity<?> getDaftarSuratEdaranTargetUnread(@PathVariable("kdJabatanTarget") String kdJabatanTarget) {
         LOGGER.info("get surat edaran target unread");
 
         return new ResponseEntity<Object>(null, HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> getSuratEdaranByKdPengumuman() {
-        LOGGER.info("get surat edaran kd pengumuman");
+    @RequestMapping(value = "/get-surat-edaran-by-kd-surat-edaran/{kdSuratEdaran}", method = RequestMethod.GET)
+    ResponseEntity<?> getSuratEdaranByKdSuratEdaran(@PathVariable("kdSuratEdaran") String kdSuratEdaran) {
+        LOGGER.info("get surat edaran kd surat edaran");
 
         return new ResponseEntity<Object>(null, HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> openSuratEdaran() {
+    @RequestMapping(value = "/open-surat-edaran/{kdSuratEdaran}", method = RequestMethod.PUT)
+    ResponseEntity<?> openSuratEdaran(@PathVariable("kdSuratEdaran") String kdSuratEdaran) {
         LOGGER.info("open surat edaran");
 
-        return new ResponseEntity<Object>(null, HttpStatus.OK);
+        return new ResponseEntity<Object>(new CustomMessage("surat edaran opened"), HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> openSuratEdaranPenilai() {
+    @RequestMapping(value = "/open-surat-edaran-penilai/{kdSuratEdaran}", method = RequestMethod.PUT)
+    ResponseEntity<?> openSuratEdaranPenilai(@PathVariable("kdSuratEdaran") String kdSuratEdaran) {
         LOGGER.info("open surat edaran penilai");
 
-        return new ResponseEntity<Object>(null, HttpStatus.OK);
+        return new ResponseEntity<Object>(new CustomMessage("surat edaran opened by penilai"), HttpStatus.OK);
 
     }
 
--- src/main/java/com/pemda/ekinerjademo/controller/api/SuratKeteranganController.java
+++ src/main/java/com/pemda/ekinerjademo/controller/api/SuratKeteranganController.java
@@ -1,14 +1,14 @@
 package com.pemda.ekinerjademo.controller.api;
 
 import com.pemda.ekinerjademo.service.SuratKeteranganService;
+import com.pemda.ekinerjademo.wrapper.input.SuratKeteranganInputWrapper;
+import com.pemda.ekinerjademo.wrapper.output.CustomMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by bayu on 18/01/18.
@@ -20,74 +20,84 @@ public class SuratKeteranganController {
     public static final Logger LOGGER = LoggerFactory.getLogger(SuratKeteranganController.class);
 
     @Autowired private SuratKeteranganService suratKeteranganService;
-    
-    ResponseEntity<?> createSuratKeterangan() {
+
+    @RequestMapping(value = "/create-surat-keterangan", method = RequestMethod.POST)
+    ResponseEntity<?> createSuratKeterangan(@RequestBody SuratKeteranganInputWrapper inputWrapper) {
         LOGGER.info("create surat keterangan");
 
-        return new ResponseEntity<Object>(null, HttpStatus.OK);
+        return new ResponseEntity<Object>(new CustomMessage("surat keterangan created"), HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> sebarSuratKeterangan() {
+    @RequestMapping(value = "/sebar-surat-keterangan/{kdSuratKeterangan}", method = RequestMethod.PUT)
+    ResponseEntity<?> sebarSuratKeterangan(@PathVariable("kdSuratKeterangan") String kdSuratKeterangan) {
+        LOGGER.info("sebar surat keterangan");
+
+        return new ResponseEntity<Object>(new CustomMessage("surat keterangan telah disebar"), HttpStatus.OK);
+
+    }
+
+    @RequestMapping(value = "/approve-surat-keterangan/{kdSuratKeterangan}", method = RequestMethod.PUT)
+    ResponseEntity<?> approveSuratKeterangan(@PathVariable("kdSuratKeterangan") String kdSuratKeterangan) {
         LOGGER.info("sebar surat keterangan");
 
         return new ResponseEntity<Object>(null, HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> approveSuratKeterangan() {
-        LOGGER.info("sebar surat keterangan");
-
-        return new ResponseEntity<Object>(null, HttpStatus.OK);
-
-    }
-
-    ResponseEntity<?> terimaSuratKeterangan() {
+    @RequestMapping(value = "/terima-surat-keterangan/{kdSuratKeterangan}", method = RequestMethod.PUT)
+    ResponseEntity<?> terimaSuratKeterangan(@PathVariable("kdSuratKeterangan") String kdSuratKeterangan) {
         LOGGER.info("terima surat keterangan");
 
         return new ResponseEntity<Object>(null, HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> getDaftarSuratKeteranganHistoryByPegawai() {
+    @RequestMapping(value = "/get-daftar-surat-keterangan-by-pembuat/{nipPembuat}", method = RequestMethod.GET)
+    ResponseEntity<?> getDaftarSuratKeteranganHistoryByPegawai(@PathVariable("nipPembuat") String nipPembuat) {
         LOGGER.info("get surat keterangan history");
 
         return new ResponseEntity<Object>(null, HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> getDaftarSuratKeteranganTarget() {
+    @RequestMapping(value = "/get-daftar-surat-keterangan-by-target/{kdJabatanTarget}", method = RequestMethod.GET)
+    ResponseEntity<?> getDaftarSuratKeteranganTarget(@PathVariable("kdJabatanTarget") String kdJabatanTarget) {
         LOGGER.info("get surat keterangan target");
 
         return new ResponseEntity<Object>(null, HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> getDaftarSuratKeteranganTargetUnread() {
+    @RequestMapping(value = "/get-daftar-surat-keterangan-by-target-unread/{kdJabatanTarget}", method = RequestMethod.GET)
+    ResponseEntity<?> getDaftarSuratKeteranganTargetUnread(@PathVariable("kdJabatanTarget") String kdJabatanTarget) {
         LOGGER.info("get surat keterangan target unread");
 
         return new ResponseEntity<Object>(null, HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> getSuratKeteranganByKdPengumuman() {
-        LOGGER.info("get surat keterangan kd pengumuman");
+    @RequestMapping(value = "/get-surat-keterangan-by-kd-surat-keterangan/{kdSuratKeterangan}", method = RequestMethod.GET)
+    ResponseEntity<?> getSuratKeteranganByKdSuratKeterangan(@PathVariable("kdSuratKeterangan") String kdSuratKeterangan) {
+        LOGGER.info("get surat keterangan kd surat keterangan");
 
         return new ResponseEntity<Object>(null, HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> openSuratKeterangan() {
+    @RequestMapping(value = "/open-surat-keterangan/{kdSuratKeterangan}", method = RequestMethod.PUT)
+    ResponseEntity<?> openSuratKeterangan(@PathVariable("kdSuratKeterangan") String kdSuratKeterangan) {
         LOGGER.info("open surat keterangan");
 
-        return new ResponseEntity<Object>(null, HttpStatus.OK);
+        return new ResponseEntity<Object>(new CustomMessage("surat keterangan opened"), HttpStatus.OK);
 
     }
 
-    ResponseEntity<?> openSuratKeteranganPenilai() {
+    @RequestMapping(value = "/open-surat-keterangan-penilai/{kdSuratKeterangan}", method = RequestMethod.PUT)
+    ResponseEntity<?> openSuratKeteranganPenilai(@PathVariable("kdSuratKeterangan") String kdSuratKeterangan) {
         LOGGER.info("open surat keterangan penilai");
 
-        return new ResponseEntity<Object>(null, HttpStatus.OK);
+        return new ResponseEntity<Object>(new CustomMessage("surat keterangan opened by penilai"), HttpStatus.OK);
 
     }
     
