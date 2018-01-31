e4f477b9d1ba21765c2d4c3bcffc7197a8a3932f
BagusJatikusuma <jatikusuma0801@gmail.com>
2018-01-31 13:25:54 +0700

    revisi 31 januari 2018 - 5

--- src/main/java/com/pemda/ekinerjademo/controller/api/TelaahanStafController.java
+++ src/main/java/com/pemda/ekinerjademo/controller/api/TelaahanStafController.java
@@ -2,12 +2,17 @@ package com.pemda.ekinerjademo.controller.api;
 
 import com.pemda.ekinerjademo.model.ekinerjamodel.SuratKuasa;
 import com.pemda.ekinerjademo.model.ekinerjamodel.TelaahanStaf;
+import com.pemda.ekinerjademo.projection.ekinerjaprojection.CustomPegawaiCredential;
+import com.pemda.ekinerjademo.repository.bismarepository.TkdUnkDao;
+import com.pemda.ekinerjademo.service.QutPegawaiCloneService;
 import com.pemda.ekinerjademo.service.SuratKuasaService;
 import com.pemda.ekinerjademo.service.TelaahanStafService;
 import com.pemda.ekinerjademo.wrapper.input.SuratKuasaInputWrapper;
 import com.pemda.ekinerjademo.wrapper.input.TelaahanStafInputWrapper;
 import com.pemda.ekinerjademo.wrapper.output.CustomMessage;
+import com.pemda.ekinerjademo.wrapper.output.SuratPerintahHistoryWrapper;
 import com.pemda.ekinerjademo.wrapper.output.TelaahanStaffHistoryWrapper;
+import com.pemda.ekinerjademo.wrapper.output.TelaahanStaffWrapper;
 import groovy.transform.Synchronized;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,6 +39,10 @@ public class TelaahanStafController {
 
     @Autowired
     private TelaahanStafService telaahanStafService;
+    @Autowired
+    private QutPegawaiCloneService qutPegawaiService;
+    @Autowired
+    private TkdUnkDao tkdUnkDao;
 
     @RequestMapping(value = "/create-telaahan-staf", method = RequestMethod.POST)
     @Synchronized
@@ -88,22 +97,72 @@ public class TelaahanStafController {
 
         List<TelaahanStaf> telaahanStafList
                 = telaahanStafService.getByNipPembuatSurat(nipPembuatSurat);
-
-        List<TelaahanStaffHistoryWrapper> telaahanStaffHistoryWrapperList
+        List<SuratPerintahHistoryWrapper> telaahanStaffHistoryList
                 = new ArrayList<>();
 
-        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
         for (TelaahanStaf telaahanStaf
                 : telaahanStafList) {
 
-            telaahanStaffHistoryWrapperList
-                    .add(new TelaahanStaffHistoryWrapper(
-                            telaahanStaf.getKdTelaahanStaf(),
-                            df.format(new Date(telaahanStaf.getTanggalPembuatanMilis())),
-                            telaahanStaf.getStatusBaca()
-                    ));
+            telaahanStaffHistoryList
+                    .add(new SuratPerintahHistoryWrapper(telaahanStaf.getKdTelaahanStaf(),
+                            "",
+                            false,
+                            telaahanStaf.getStatusBaca(),
+                            "telaahan staff",
+                            14,
+                            telaahanStaf.getTanggalPembuatanMilis(),
+                            telaahanStaf.getStatusPenilaian()));
         }
 
-        return new ResponseEntity<Object>(telaahanStaffHistoryWrapperList, HttpStatus.OK);
+        return new ResponseEntity<Object>(telaahanStaffHistoryList, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/get-telaahan-staff-by-kd-telaahan-staff/{kdTelaahanStaff}", method = RequestMethod.GET)
+    ResponseEntity<?> getTelaahanStaffByKdTelaahanStaff(
+            @PathVariable("kdTelaahanStaff") String kdTelaahanStaff) {
+        LOGGER.info("get telaahan staff by kd telaahan staf");
+
+        TelaahanStaf telaahanStaf = telaahanStafService.getTelaahanStaf(kdTelaahanStaff);
+
+        List<CustomPegawaiCredential> qutPegawaiList
+                = qutPegawaiService.getCustomPegawaiCredentials();
+
+        CustomPegawaiCredential penandatangan = null,
+                                pembuatSurat  = null;
+
+        for (CustomPegawaiCredential pegawai : qutPegawaiList) {
+            if (telaahanStaf.getNipPenandatangan()
+                    .equals(pegawai.getNip())) {
+                penandatangan = pegawai;
+
+                break;
+            }
+        }
+
+        for (CustomPegawaiCredential pegawai : qutPegawaiList) {
+            if (telaahanStaf.getNipPembuatSurat()
+                    .equals(pegawai.getNip())) {
+                pembuatSurat = pegawai;
+
+                break;
+            }
+        }
+
+        TelaahanStaffWrapper telaahanStaffWrapper
+                = new TelaahanStaffWrapper(
+                        telaahanStaf.getKdTelaahanStaf(),
+                        telaahanStaf.getTentang(),
+                        telaahanStaf.getPersoalan(),
+                        telaahanStaf.getPraanggapan(),
+                        telaahanStaf.getFaktaYangMempengaruhi(),
+                        telaahanStaf.getAnalisis(),
+                        telaahanStaf.getSimpulan(),
+                        telaahanStaf.getSaran(),
+                        penandatangan,
+                        telaahanStaf.getTanggalPembuatanMilis(),
+                        pembuatSurat,
+                        tkdUnkDao.findOne(pembuatSurat.getUnitKerja()).getUnitKerja());
+
+        return new ResponseEntity<Object>(telaahanStaf, HttpStatus.OK);
     }
 }
